910c9389e36ef8a05a584c62506d9e7f41811e0f
chendajian <chen.dajian@aliyun.com>
2023-03-14 09:44:46 +0800

    支持开发环境解决跨域问题

---
+++ src/components/Cors.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {onMounted} from "vue";
+import axios from "axios"
+
+onMounted(()=>{
+  console.log("onmounted")
+
+  // axios.get("https://api.uomg.com/api/rand.avatar?sort=男&format=json")
+  axios.get("/api/FingerUnion/list.php")
+      .then(res => {
+        console.log("data: "+ JSON.stringify(res))
+      })
+})
+</script>
+
+<template>
+<h3>跨域解决方案</h3>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   server: {
     port: 8080,
     open: false, // true表示自动打开浏览器
-    hmr: true // 热更新
+    hmr: true, // 热更新
+    proxy: {
+      '/api/': {
+        target: 'http://iwenwiki.com/aaa',
+        changeOrigin: true
+      }
+    }
   },
   plugins: [vue()],
 })
