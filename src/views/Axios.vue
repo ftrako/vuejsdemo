7b24a1fed1ae483f0f750ff3192530dcc66b3b01
chendajian <chen.dajian@aliyun.com>
2023-03-16 15:56:48 +0800

    增加.env配置文件

--- src/views/Axios.vue
+++ src/views/Axios.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <input v-model="data.domain"  type="text" />
+  </div>
+  <div>
+    data.domain: {{ data.domain }}
+  </div>
+  <div>
     data: {{ data }}
   </div>
 
@@ -10,7 +16,7 @@
 </template>
 
 <script setup>
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import icp from "../api/icp/icp.js"
 
 let data = reactive({
@@ -20,7 +26,7 @@ let data = reactive({
 })
 
 const post = () => {
-  icp.query("https://www.baidu.com").then((response) => {
+  icp.query(data.domain).then((response) => {
     console.log("data:", data)
     console.log("response:", response)
     data.domain = response.data.domain
