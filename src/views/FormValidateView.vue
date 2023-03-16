7acb9ab4d8fb8d6ca4baf34679e8f6a30d9c2f5e
chendajian <chen.dajian@aliyun.com>
2023-03-16 08:59:12 +0800

    增加表单校验提示

---
+++ src/views/FormValidateView.vue
@@ -0,0 +1,36 @@
+<template>
+  <h3>这是Form Validate示例</h3>
+
+  <el-form
+      :model = "form"
+      :rules="rules"
+      label-width="80px"
+      style="max-width: 460px"
+  >
+    <el-form-item label="姓名" prop="name">
+      <el-input v-model="form.name" clearable style="width: 240px"/>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup>
+import {reactive} from "vue";
+
+let form = reactive({
+  name: ''
+})
+const rules = {
+  name: [
+    {
+      required: true,
+      message: "请输入姓名",
+      trigger: "blur"
+    },
+    {
+      pattern: /^123$/,
+      message: "请输入正确的姓名",
+      trigger: "blur"
+    }
+  ]
+}
+</script>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,38 +1,84 @@
 <template>
   <div>
-    id={{ obj.id }}
+    data.obj.count = {{ data.obj.count  }}
   </div>
   <div>
-    name={{ obj.name }}
+    obj.count = {{ obj.count  }}
+  </div>
+  <div>
+    age = {{ age  }}
+  </div>
+  <div>
+    data.age = {{ data.age  }}
   </div>
 
   <div>
-    <button @click="change">修改</button>
+    <button @click="onClicked">增加</button>
   </div>
-
-  <cascader-view />
+<!--  <div>-->
+<!--    <button @click="onClicked2">增加data.age</button>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <button @click="onClicked3">增加data.obj.count</button>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <button @click="onClicked4">增加obj.count</button>-->
+<!--  </div>-->
 </template>
 
 <script setup>
-import {reactive, ref, toRefs} from "vue"
+import {reactive, ref, toRef, toRefs} from "vue"
 import CascaderView from "./CascaderView.vue";
+import FormValidateView from "./FormValidateView.vue";
+
 
 let data = reactive({
   obj: {
     id: "1001",
-    name: "zhangsan"
-  }, age: "13"
+    name: "zhangsan",
+    count: 0
+  }, age: 0
 })
 
-let {obj, age} = toRefs(data)
+let refData = ref({
+  count: 0
+})
 
-const change = () => {
-  obj.value.id = "2002"
-  obj.value.name = "lisi"
+let {age, obj} = toRefs(data)
 
-  console.log("obj:", obj)
-  console.log("obj.value:", obj.value)
-  console.log("data", data)
-  console.log("data.value:", data.value)
+const onClicked = ()=>{
+  // console.log("data.age=",data.age)
+  // console.log("age.value=",age.value)
+  console.log("refData.value.count=",refData.value.count)
+  console.log("refData=",refData)
+  refData.value.count++
 }
+// const onClicked2 = ()=>{
+//   console.log("data.age=",data.age)
+//   console.log("age.value=",age.value)
+//   console.log("data=",data)
+//   data.age++
+// }
+// const onClicked3 = ()=>{
+//   console.log("data.obj.count=",data.obj.count)
+//   console.log("age.value=",age.value)
+//   console.log("obj=",obj)
+//   console.log("data=",data)
+//   console.log("data.obj=",data.obj)
+//   data.obj.count++
+//   age.value++
+// }
+// const onClicked4 = ()=>{
+//   console.log("data.obj.count=",data.obj.count)
+//   console.log("age.value=",age.value)
+//   console.log("data=",data)
+//   console.log("data.obj=",data.obj)
+//   console.log("refObj=",refObj)
+//   obj.value.count++
+//   age.value++
+// }
+
+
+
+
 </script>
